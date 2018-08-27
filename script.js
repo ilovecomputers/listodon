1656c360073240afac0c13d9b8386e5dd0564efb
Glitch (mastoviz) <none>
2018-08-27 09:32:03 +0000

    🐉🍹 Checkpoint ./index.html:443642/2129 ./script.js:443642/1072

--- script.js
+++ script.js
@@ -30,18 +30,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var client_id = localStorage.getItem("MASTODON_CLIENT_ID");
   var client_secret = localStorage.getItem("MASTODON_CLIENT_SECRET");
   if (window.location.href.indexOf("?code=") !== -1 && mastodon_url != "" && client_id != "" && client_secret != "") {
+    elem("mastodon_url").value = mastodon_url;
     var code = window.location.href.replace(window.location.origin+window.location.pathname+"?code=", "");
     var url2 = mastodon_url+"/oauth/token";
-    elem("mastodon_url").value = mastodon_url;
-    elem("client_id").textContent = client_id;
-    elem("client_secret").textContent = client_secret;
     var args2 = {client_id: client_id,
                  client_secret: client_secret,
                  redirect_uri: redirect_uri,
                  grant_type: "authorization_code",
                  code: code};
     post(url2, args2, function(data) {
-      elem('access_token').textContent = data.access_token;
+      localStorage.setItem("MASTODON_ACCESS_KEY", data.access_token);
     })
   } else {
     localStorage.setItem("MASTODON_URL", "");
@@ -52,10 +50,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
     event.preventDefault();
     var url = elem("mastodon_url").value+"/api/v1/apps";
     var s = elem("scopes");
-    var scopes = s.options[s.selectedIndex].value;
-    var args = {client_name: elem("client_name").value,
+    var scopes = "read";
+    var args = {client_name: "Mastoviz",
                 redirect_uris: redirect_uri,
-                website: elem("website").value,
+                website: "https://mastoviz.glitch.me/",
                 scopes: scopes};
     post(url ,args, function(data) {
       localStorage.setItem("MASTODON_URL", elem("mastodon_url").value);
@@ -66,17 +64,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     });
   });
   elem("btn_clr").addEventListener("click", function(event) {
-    elem('access_token').textContent = "";
-    elem('client_id').textContent = "";
-    elem('client_secret').textContent = "";
     localStorage.setItem("MASTODON_URL", "");
     localStorage.setItem("MASTODON_CLIENT_ID", "");
     localStorage.setItem("MASTODON_CLIENT_SECRET", "");
-  });
-  elem("btn_client_secret").addEventListener("click", function(event) {
-    elem("client_secret").style.visibility = "visible";
-  });
-  elem("btn_client_id").addEventListener("click", function(event) {
-    elem("client_id").style.visibility = "visible";
   });
 });
