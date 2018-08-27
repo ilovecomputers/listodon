4908a3fcfb9190b0196e1fc0c8860134317b0fcb
Glitch (mastoviz) <none>
2018-08-27 09:42:37 +0000

    😋👐 Checkpoint ./script.js:443642/2209

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
-  function elem(el) {
+function elem(el) {
     return document.getElementById(el);
-  }
-  function makePostData(args) {
+}
+
+function makePostData(args) {
     var data = [];
     Object.keys(args).forEach(function(key) {
-      data.push(encodeURIComponent(key) + '=' + encodeURIComponent(args[key]));
+        data.push(encodeURIComponent(key) + '=' + encodeURIComponent(args[key]));
     });
     return data.join('&').replace(/%20/g, '+');
-  }
-  function post(url, args, done) {
+}
+
+function post(url, args, done) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        var data = xhr.response;
-        done(data);
-      }
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var data = xhr.response;
+            done(data);
+        }
     }
     xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type" , "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.responseType = "json";
     var postData = makePostData(args);
     xhr.send(postData);
-  }
+}
+
+function get(url, done) {
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      var data = JSON.parse(request.responseText);
+      done(data);
+    } else {
+      console.log("Error with get request");
+    }
+  };
+
+  request.onerror = function() {
+    // There was a connection error of some sort
+  };
+
+  request.send();
+}
 
 document.addEventListener("DOMContentLoaded", function(event) {
   /* main */
